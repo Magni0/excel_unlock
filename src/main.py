cf4c7bd4183b9178bc3cd00bb3cd08bf357e5d5c
Logan Harris <loganrodharris@gmail.com>
2022-12-14 22:14:00 +1100

    finished convertion to zip and extraction

--- src/main.py
+++ src/main.py
@@ -1,49 +1,64 @@
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from os import listdir, getcwd, mkdir
+from os import listdir, getcwd, mkdir, remove
 from tqdm import tqdm
 import zipfile
+import shutil
 
 class UnlockExcel:
     def __init__(self, args):
         self.args = args
-        self.path = self.args.path
+        self.file = self.args.file
         self.output = self.args.output
-        # self.replace = self.args.replace
+        self.replace = self.args.replace
         # self.unhide = self.args.unhide
-        self.file_type = "xlsx" # have original file type so it's converted back to the same either xlsx or xls
+        self.file_type = "xlsx" # have original file type so it's converted back to the same either xlsx or xls.
+        self.cwd = getcwd()
 
     def convert_to_zip(self):
-        """Converts the xls/xlsx file into a zip file"""
+        """Converts the xls/xlsx file into a zip file and extracts."""
+        self.file_type = self.file.split('.')[1]
 
-        pass
+        zip_name = f"{self.file.split('.')[0]}.zip"
+        shutil.copy(self.file, zip_name) # create copy zip file
+        
+        with zipfile.ZipFile(zip_name, 'r') as zip_ref:
+            zip_ref.extractall(f"{self.cwd}/temp_{zip_name.split('.')[0]}") # unzip file to edit
+            self.temp_dir = f"{self.cwd}/temp_{zip_name.split('.')[0]}"
+        
+        remove(zip_name)
+        if self.replace:
+            remove(self.file)
 
     def convert_to_excel(self):
-        """Converts the file back into xls/xlsx"""
+        """Converts back into xls/xlsx and deletes directory."""
         
         pass
 
     def remove_protection(self):
-        """Removes protect tags from sheets and workbook"""
+        """Removes protect tags from sheets and workbook."""
 
         pass
 
     def unhide_sheets(self):
-        """Removes hide tags from sheets and rows/columns"""
+        """Removes hide tags from sheets and rows/columns."""
 
         pass
 
     def unlock_file(self):
-        """Unlocks the excel file"""
+        """Unlocks the excel file."""
 
         pass
 
 parser = ArgumentParser(
     formatter_class=RawDescriptionHelpFormatter, 
-    description="""Unlocks excel spreadsheets and workbooks by going into the XML to remove the lock tags"""
+    description="""Unlocks excel spreadsheets and workbooks by going into the XML to remove the lock tags."""
 )
 
-parser.add_argument("-p", "--path", nargs="?", const=getcwd(), type=str, help="The path of the excel to unlock including the file name eg. path/to/file/filename.xlsx (path is cwd by default)")
-parser.add_argument("-o", "--output", nargs="?", const=getcwd(), type=str, help="Output directory of the unlocked excel file")
-# parser.add_argument("-r", "--replace", action="store_true", help="replaces the excel file instead of making a copy with _unlocked appended")
+parser.add_argument("-f", "--file", required=True, type=str, help="The file path (eg. path/to/file/filename.xlsx).")
+parser.add_argument("-o", "--output", const=None, required=False, type=str, help="Output name and path of the unlocked excel file.")
+parser.add_argument("-r", "--replace", action="store_true", help="Replaces the excel file instead of making a copy with _unlocked appended")
 # parser.add_argument("-u", "--unhide", action="store_true", help="unhides all sheets and rows/columns")
 args = parser.parse_args()
+
+instance = UnlockExcel(args)
+instance.convert_to_zip()
